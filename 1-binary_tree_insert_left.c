ba55b306f19f26dd3041d33f30d1dad64127ae31
ismark <akroutismail01@gmail.com>
2024-03-26 08:00:13 -0700

    1-2

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -14,12 +14,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	new_n = malloc(sizeof(binary_tree_t));
 	if (new_n == NULL)
 		return (NULL);
-	if (parent == NULL)
-		return (NULL);
-	new_n->parent = parent;
-	new_n->left = NULL;
-	new_n->right = NULL;
-	new_n->n = value;
+	new_n = binary_tree_node(parent, value);
 	if (parent->left != NULL)
 	{
 		new_n->left = parent->left;
