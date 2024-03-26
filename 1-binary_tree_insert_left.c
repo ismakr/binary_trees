51412aa781008c1eeaceaa6774addd27a0e50d8c
ismark <akroutismail01@gmail.com>
2024-03-26 08:32:43 -0700

    1-2

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -11,6 +11,8 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_n;
 
+	if (parent == NULL)
+		return (NULL);
 	new_n = binary_tree_node(parent, value);
 	if (new_n == NULL)
 		return (NULL);
