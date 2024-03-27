5b8f156cf0ff8adae66b888fc3015022ae5229c6
ismark <akroutismail01@gmail.com>
2024-03-27 15:18:13 -0700

    10-0

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_depth -  measures the depth of a binary tree
+ * @tree: tree
+ * Return: n depth
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	const binary_tree_t *ptr;
+	size_t n;
+
+	if (tree == NULL)
+		return (0);
+	n = 0;
+	ptr = tree;
+	while (ptr-parent)
+	{
+		ptr = ptr->parent;
+		n += 1;
+	}
+	return (n);
+}
