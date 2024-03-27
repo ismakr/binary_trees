9b3e9ef66fa18cbc52c72e4f675ec7f8b74ed3ad
ismark <akroutismail01@gmail.com>
2024-03-27 08:16:50 -0700

    9-0

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_height -  measures the height of a binary tree
+ * @tree: tree
+ * Return: n hieght
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	const binary_tree_t *ptr;
+	size_t n;
+
+	n = -1;
+	ptr = tree;
+	while (ptr)
+	{
+		if (ptr->right == NULL)
+			ptr = ptr->left;
+		else if (ptr->left == NULL)
+			ptr = ptr->right;
+		else
+			ptr = ptr->left;
+		n += 1;
+	}
+	return (n);
+}
