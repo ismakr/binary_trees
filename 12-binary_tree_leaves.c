ba6df73997389179ec29e58d1d1ca7f52f1224c8
ismark <akroutismail01@gmail.com>
2024-03-28 08:10:12 -0700

    12-1

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -8,9 +8,8 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
-	int l;
-
-	l = binary_tree_leaves(tree->left);
-	binary_tree_leaves(tree->right);
-	return (l + 1);
+	else if (tree->left == NULL && tree->right == NULL)
+		return (1);
+	else
+		return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
