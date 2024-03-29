72b5548297584a65c02ac563bf57a424c609514d
ismark <akroutismail01@gmail.com>
2024-03-29 07:15:18 -0700

    11-2

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -6,34 +6,7 @@
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	const binary_tree_t *pright;
-	const binary_tree_t *pleft;
-	size_t n;
-
 	if (tree == NULL)
 		return (0);
-	n = 1;
-	pright = tree->right;
-	pleft = tree->left;
-	while (pright)
-	{
-		if (pright->right == NULL)
-			pright = pright->left;
-		else if (pright->left == NULL)
-			pright = pright->right;
-		else
-			pright = pright->right;
-		n += 1;
-	}
-	while (pleft)
-	{
-		if (pleft->left == NULL)
-			pleft = pleft->right;
-		else if (pleft->right == NULL)
-			pleft = pleft->left;
-		else
-			pleft = pleft->left;
-		n += 1;
-	}
-	return (n);
+	return (1 + binary_tree_size(tree->right) + binary_tree_size(tree->left));
 }
