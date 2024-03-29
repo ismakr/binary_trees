879546d32897f1e76987d5ceb8efbbb9c797b496
ismark <akroutismail01@gmail.com>
2024-03-29 07:20:45 -0700

    16-1

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,42 +1,27 @@
 #include "binary_trees.h"
 /**
+ * binary_tree_size - measures the size of a binary tree
+ * @tree: tree
+ * Return: n size
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	return (1 + binary_tree_size(tree->right) + binary_tree_size(tree->left));
+}
+/**
  * binary_tree_is_perfect - checks if a binary tree is perfect
  * @tree: tree
  * Return: 1 if perfect 0 otherwise
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	const binary_tree_t *pright;
-	const binary_tree_t *pleft;
 	int l, r;
 
-	if (tree == NULL)
-		return (0);
-	r = 0;
-	l = 0;
-	pright = tree->right;
-	pleft = tree->left;
-	while (pright)
-	{
-		if (pright->right == NULL)
-			pright = pright->left;
-		else if (pright->left == NULL)
-			pright = pright->right;
-		else
-			pright = pright->right;
-		l += 1;
-	}
-	while (pleft)
-	{
-		if (pleft->left == NULL)
-			pleft = pleft->right;
-		else if (pleft->right == NULL)
-			pleft = pleft->left;
-		else
-			pleft = pleft->left;
-		r += 1;
-	}
-	if (r == l)
+	l = binary_tree_size(tree->left);
+	r = binary_tree_size(tree->right);
+	if (l == r)
 		return (1);
 	else
 		return (0);
